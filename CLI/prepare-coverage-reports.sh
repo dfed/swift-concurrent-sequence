40b239f4c061be6d134acb16a6176b9b4bd0a049
Dan Federman <dfed@me.com>
2025-08-31 22:38:24 -0700

    Improve coverage script (#6)
    Removes errant path

--- CLI/prepare-coverage-reports.sh
+++ CLI/prepare-coverage-reports.sh
@@ -14,7 +14,7 @@ function exportlcov() {
 	elif [[ -z $executable ]]; then
 		echo "\tAborting creation of $output_file_name – no executable found."
 	else
-		output_dir=".build/artifacts/$build_type"
+		output_dir=".build/artifacts/"
 		mkdir -p $output_dir
 
 		output_file="$output_dir/$output_file_name"
